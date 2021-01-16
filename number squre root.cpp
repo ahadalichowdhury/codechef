f5677c79e25c58eb84b135110e689c04d1517a68
homoant <77457836+homoant@users.noreply.github.com>
2021-01-16 11:51:03 +0600

    Create number squre root.cpp

---
+++ number squre root.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int t;
+	int a;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		a =sqrt(n);
+		cout<<a<<endl;
+	    
+	}
+	return 0;
+}
